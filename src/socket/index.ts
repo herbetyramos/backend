71de80df49ebdb1488e57f163d173c3a8a3b08e3
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-09-15 18:43:33 -0300

    index do socket

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,38 +1,68 @@
+
 import { Server } from "socket.io";
 
 let io: Server;
 
+const allowedOrigins = [
+  "https://gestaom.com",
+  "https://www.gestaom.com",
+  "http://localhost:4000",
+  "http://192.168.15.84:4000",
+  "http://192.168.15.84:3001",
+];
+
 export function initSocket(server: any) {
+  io = new Server(server, {
+    cors: {
+      origin: allowedOrigins,
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+    transports: ["websocket", "polling"],
+  });
 
-    io = new Server(server, {
-        cors: {
-            origin: "*",
-            methods: ["GET", "POST"]
-        }
+  io.on("connection", (socket) => {
+    console.log("Cliente Socket.IO conectado:", socket.id);
+
+    socket.on("join", (conversaId: string) => {
+      if (!conversaId) {
+        return;
+      }
+
+      socket.join(conversaId);
+
+      console.log(
+        `Socket ${socket.id} entrou na conversa ${conversaId}`
+      );
     });
 
-    io.on("connection", (socket) => {
+    socket.on("leave", (conversaId: string) => {
+      if (!conversaId) {
+        return;
+      }
 
-        console.log("Cliente conectado:", socket.id);
+      socket.leave(conversaId);
 
-        socket.on("join", (conversaId: string) => {
-            socket.join(conversaId);
-        });
-
-        socket.on("disconnect", () => {
-            console.log("Cliente desconectado:", socket.id);
-        });
-
+      console.log(
+        `Socket ${socket.id} saiu da conversa ${conversaId}`
+      );
     });
 
-    return io;
+    socket.on("disconnect", (reason) => {
+      console.log(
+        `Cliente Socket.IO desconectado: ${socket.id}`,
+        reason
+      );
+    });
+  });
+
+  return io;
 }
 
 export function getIO() {
+  if (!io) {
+    throw new Error("Socket.IO não inicializado");
+  }
 
-    if (!io) {
-        throw new Error("Socket.IO não inicializado");
-    }
-
-    return io;
+  return io;
 }
